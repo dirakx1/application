16fd27d09cf835dd9dfce86e5b05d11c3a2772ba
rafael ortiz <35827025+dirakx1@users.noreply.github.com>
2019-08-02 08:47:05 -0500

    Update vars.tf

--- devops/terraform/vars.tf
+++ devops/terraform/vars.tf
@@ -1,1 +1,51 @@
+# variables for project
 
+variable "project_id" {
+  default = ""
+}
+
+variable "billing_account_id" {
+  default = "yyyyyyyyyyyyyyyyyyyyyyyyyy"
+}
+
+variable "environment" {
+  default = "prod"
+}
+
+variable "project_name" {
+  default = ""
+}
+
+variable "cluster_name" {
+  default = "timeoff-cluster"
+}
+
+variable "network" {    # Cluster shared network
+  default = "shared-net"
+}
+
+variable "subnetwork" {   # Cluster subnet varies between envs
+  default = "k8-nodes"
+}
+
+variable "pool_name" {
+  default = "timeoff-node-pool"
+}
+
+variable "description" {
+  default = "timeoff"
+}
+
+variable "apis" {
+  default = ["bigquery-json.googleapis.com",
+    "pubsub.googleapis.com",
+    "cloudfunctions.googleapis.com",
+    "dataflow.googleapis.com",
+    "sqladmin.googleapis.com",
+    "container.googleapis.com",
+  ]
+}
+
+variable "machine_type" {
+  default = "n1-standard-4"
+} # cluster machine_type
